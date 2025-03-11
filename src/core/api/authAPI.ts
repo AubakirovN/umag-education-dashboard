3781529fa3f3c1079354bd5705b2fdc68731c1f7
kazba <kazba_anuarbekov@gmail.com>
2025-03-11 13:53:47 +0500

    fixed

--- src/core/api/authAPI.ts
+++ src/core/api/authAPI.ts
@@ -1,30 +1,27 @@
 import {
   LoginDto,
-  LoginOutputDto,
   SetPasswordDto,
-  SetPasswordOutputDto,
-  UserOutputDto,
 } from "@/core/types";
 import axios from "axios";
 import { baseUrl } from "../constant";
 
-export const login = async (body: LoginDto): Promise<LoginOutputDto> => {
-  const response = await axios.post<LoginOutputDto>(
+export const login = async (body: LoginDto): Promise<any> => {
+  const response = await axios.post<any>(
     `${baseUrl}/auth/login`,
     body
   );
   return response.data;
 };
 
-export const getUserInfo = async (): Promise<UserOutputDto> => {
-  const response = await axios.get<UserOutputDto>(`${baseUrl}/auth/me`);
+export const getUserInfo = async (): Promise<any> => {
+  const response = await axios.get<any>(`${baseUrl}/auth/me`);
   return response.data;
 };
 
 export const setPassword = async (
   body: SetPasswordDto
-): Promise<SetPasswordOutputDto> => {
-  const response = await axios.post<SetPasswordOutputDto>(
+): Promise<any> => {
+  const response = await axios.post<any>(
     `${baseUrl}/auth/set-password`,
     body
   );
